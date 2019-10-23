c2aca5c49f9d76585f4c995126787d86733c3541
Brandon Weaver <a7xncob@gmail.com>
2019-10-23 01:26:07 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,36 +1,97 @@
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
+  
+  index = 0
+  while index < collection.size do
+    if collection[index][:item] == name
+      return collection[index]
+    end
+    index += 1
+  end
+  
+  return nil
+end
+
+def unique_count(cart)
+  temp = []
+  
+  index = 0
+  while index < cart.size do
+    if !temp.include?(cart[index][:item])
+      temp << cart[index][:item]
+    else
+      cart[index][:count] += 1
+    end
+    index += 1
+  end
+  
+  return cart.uniq
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  
+  index = 0
+  while index < cart.size do
+    cart[index][:count] = 1
+    index += 1
+  end
+  
+  return unique_count(cart)
 end
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  temp = cart
+  
+  index = 0
+  while index < coupons.size do
+    discounted_price = coupons[index][:cost] / coupons[index][:num]
+    
+    i = 0
+    while i < cart.size do
+      if coupons[index][:item] == temp[i][:item] && temp[i][:count] >= coupons[index][:num]
+        temp[i][:count] -= coupons[index][:num]
+        temp << { item: "#{temp[i][:item]} W/COUPON", price: discounted_price, clearance: temp[i][:clearance], count: coupons[index][:num] }
+      end
+      i += 1
+    end
+    
+    index += 1
+  end
+  
+  return temp
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  temp = cart
+  
+  index = 0
+  while index < temp.size do
+    if temp[index][:clearance]
+      discount = temp[index][:price] * 20 / 100
+      temp[index][:price] -= discount
+    end
+    index += 1
+  end
+  
+  return temp
 end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+  consolidated_cart = consolidate_cart(cart)
+  cart_after_coupons = apply_coupons(consolidated_cart, coupons)
+  discounted_cart = apply_clearance(cart_after_coupons)
+  
+  total = 0
+  
+  index = 0
+  while index < discounted_cart.size do
+    total += discounted_cart[index][:count] * discounted_cart[index][:price]
+    index += 1
+  end
+  
+  if total > 100
+    discount = total * 10 / 100
+    total -= discount
+  end
+  
+  return total
 end
