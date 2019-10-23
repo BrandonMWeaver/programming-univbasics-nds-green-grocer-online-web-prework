cc364c6363d13808d55028e68b4cb318fc93d5f7
Brandon Weaver <a7xncob@gmail.com>
2019-10-23 00:41:21 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -49,7 +49,9 @@ def apply_coupons(cart, coupons)
     while i < cart.size do
       if coupons[index][:item] == temp[i][:item] && temp[i][:count] >= coupons[index][:num]
         temp[i][:count] -= coupons[index][:num]
-        temp << { item: temp[i][:item], price: temp[i][:price], clearance: temp[i][:clearance], count: coupons[index][:num] }
+        temp << { item: "#{temp[i][:item]} W/COUPON", price: temp[i][:price], clearance: temp[i][:clearance], count: coupons[index][:num] }
+        if temp[i][:count] == 0
+          temp[i].remove
       end
       i += 1
     end
